df5f6c39e8cc9b04e7f282f3d5f08946166f9162
SITHUWA11 <Wadagaming14@gmail.com>
2024-12-07 14:45:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WzJAnaZQ#v0IUp5ItquL4JuLm_O8ry_TB-TIwfSb2TBmBYmcAE7c' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=njYkADzA#6AvEJv0kZPNCgl4uRHKkZ4suIE6PaH_fVg_aNfVZu6U' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
